453732a9bf61f1a0b671c8cac50f607dbc7c054d
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-08-18 20:03:32 +0530

    Remove un used constructor

--- src/components/loader.js
+++ src/components/loader.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import loadingGif from '../assets/loader.gif';
 
 export default class Loader extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <div>
